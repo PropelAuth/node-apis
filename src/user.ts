f1177d11a84620075827f68578071fcf529da4cc
Andrew Israel <andrew@propelauth.com>
2023-08-01 14:46:06 -0700

    Switch unknown to any

--- src/user.ts
+++ src/user.ts
@@ -13,7 +13,7 @@ export type User = {
     legacyUserId?: string
     impersonatorUserId?: string
     metadata?: {[key: string]: any}
-    properties?: {[key: string]: any}
+    properties?: {[key: string]: unknown}
 }
 
 export type Org = {
@@ -48,7 +48,7 @@ export type UserMetadata = {
     legacyUserId?: string
     impersonatorUserId?: string
     metadata?: {[key: string]: any}
-    properties?: {[key: string]: any}
+    properties?: {[key: string]: unknown}
 }
 
 export class OrgMemberInfo {
