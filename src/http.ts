6ba8652188d204b761a7dce11f40bd07d836b971
Andrew Israel <andrew@propelauth.com>
2023-09-18 17:54:57 -0700

    Switch from https to fetch for better edge support

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,3 @@
-import http from "https"
-
 export type HttpResponse = {
     statusCode?: number
     response: string
@@ -13,45 +11,20 @@ export function httpRequest(
     body?: string
 ): Promise<HttpResponse> {
     let headers: any = {
-        Authorization: "Bearer " + apiKey,
+        "Authorization": "Bearer " + apiKey,
         "Content-Type": "application/json",
     }
-    if (body !== undefined) {
-        headers["Content-Length"] = Buffer.byteLength(body)
-    }
 
-    return new Promise((resolve, reject) => {
-        const req = http.request(
-            {
-                protocol: authUrlOrigin.protocol,
-                hostname: authUrlOrigin.hostname,
-                port: authUrlOrigin.port,
-                path: path,
-                method: method,
-                headers: headers,
-            },
-            (res) => {
-                let response = ""
-                res.on("data", (data) => {
-                    response += data
-                })
-                res.on("end", () => {
-                    resolve({
-                        statusCode: res.statusCode,
-                        response: response,
-                    })
-                })
+    return fetch(authUrlOrigin.origin + path, {
+        method,
+        headers,
+        body,
+    }).then(response => {
+        return response.text().then(res => {
+            return {
+                statusCode: response.status,
+                response: res,
             }
-        )
-
-        req.on("error", (error) => {
-            reject(error)
         })
-
-        if (body !== undefined) {
-            req.write(body)
-        }
-
-        req.end()
     })
 }
